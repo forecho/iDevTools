fe92232fddd5e34d022eb84c576f943a335e6ae0
forecho <caizhenghai@gmail.com>
2017-06-08 18:13:34 +0800

    添加导航和home组件

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  {path: '', children: []},
+  {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
