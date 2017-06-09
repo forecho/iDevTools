b6142350e4aaeb98de67012be162ab0e5b82c00f
forecho <caizhenghai@gmail.com>
2017-06-09 22:18:06 +0800

    加密模块完成

--- src/app/encrypt/encrypt.component.ts
+++ src/app/encrypt/encrypt.component.ts
@@ -10,7 +10,14 @@ import * as Hashes from 'jshashes';
 export class EncryptComponent implements OnInit {
 
   form;
-  string: string = new Hashes.MD5().hex('String'); // 结果初始化数据
+  string: string = new Hashes.MD5().hex('iDevTools.org'); // 结果初始化数据
+  type: string;
+
+  radioData = [
+    {label: 'MD5', value: 'md5'},
+    {label: 'SHA1', value: 'sha1'},
+    {label: 'SHA256', value: 'sha256'},
+  ];
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
@@ -20,12 +27,26 @@ export class EncryptComponent implements OnInit {
 
   ngOnInit() {
     this.form = new FormGroup({
-      string: new FormControl('String')
+      string: new FormControl('iDevTools.org'), //表单初始值
+      type: new FormControl('md5'), // 表单初始值
     })
   }
 
   onSubmit = function (form) {
-    this.string = new Hashes.MD5().hex(form.string);
+    switch (form.type) {
+      case 'sha1':
+        this.string = new Hashes.SHA1().hex(form.string);
+        break;
+
+      case 'sha256':
+        this.string = new Hashes.SHA256().hex(form.string);
+        break;
+
+      default:
+        this.string = new Hashes.MD5().hex(form.string);
+        break;
+    }
+    this.type = form.type;
   };
 
   clipCallback = function (bool) {
